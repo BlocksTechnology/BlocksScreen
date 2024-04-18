8a84a6b82564abeca30a1de79514c6af84b6db45
Gleb-blocksdev <g.lemos@blockstec.com>
2024-04-18 15:23:16 +0200

    Print Tab Updates

--- panels/printTab.py
+++ panels/printTab.py
@@ -13,8 +13,15 @@ class PrintTab(QStackedWidget):
         self.panel.setupUi(self)
         self.show()
         
+        self.index_stack = []
         
-        # self.panel.print_btn.clicked()
-
+        # Connecting the print_btn.clicked event to the change_page method
+        self.panel.main_print_btn.clicked.connect(self.change_page)
+        self.panel.files_back_folder_btn.clicked.connect(self.change_page)
+        
     
+    def change_page(self, int):
+        self.setCurrentIndex(1)
+        self.index_stack.append(self.currentIndex())
+        
     
