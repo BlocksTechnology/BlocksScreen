63217c4ff1aff78dfca2d86727eed5aac86df239
HugoCLSC <hugo.santos.costa@gmail.com>
2025-05-22 15:14:34 +0100

    Added marquee effect when labels texts are bigger thant the widget size

--- BlocksScreen/utils/blocks_label.py
+++ BlocksScreen/utils/blocks_label.py
@@ -8,23 +8,37 @@ class BlocksLabel(QtWidgets.QLabel):
             super(BlocksLabel, self).__init__(parent, *args, **kwargs)
 
         self.icon_pixmap: typing.Optional[QtGui.QPixmap] = None
-        self._text: typing.Optional[str] = None
+        self._text: str = ""
         self._background_color: typing.Optional[QtGui.QColor] = None
         self._rounded: bool = False
+        self._marquee: bool = False
+        self.timer: QtCore.QTimer = QtCore.QTimer()
+        self.timer.timeout.connect(self._scroll_text)
+        self.scroll_pos = 0
+        self.paused = False
+        self.scroll_speed = 20
+        self.scroll_animation_speed = 50
+
+        self.setMouseTracking(True)
+        self.setSizePolicy(
+            QtWidgets.QSizePolicy.Policy.MinimumExpanding,
+            QtWidgets.QSizePolicy.Policy.Fixed,
+        )
 
     def parent(self) -> QtCore.QObject:
         return super().parent()
 
+    def resizeEvent(self, a0: QtGui.QResizeEvent) -> None:
+        self.update_text_metrics()
+        return super().resizeEvent(a0)
+
     def setPixmap(self, a0: QtGui.QPixmap) -> None:
         self.icon_pixmap = a0
-        self.update()
-        return
+        self.repaint()
 
-    def setText(self, a0: str) -> None:
-        self._text = a0
-        self.update()
-
-        return super().setText(a0)
+    def setText(self, text: str) -> None:
+        self._text = text
+        self.update_text_metrics()
 
     @property
     def background_color(self) -> typing.Optional[QtGui.QColor]:
@@ -43,8 +57,15 @@ class BlocksLabel(QtWidgets.QLabel):
     def rounded(self, on: bool) -> None:
         self._rounded = on
 
-    # TODO: Add rounded object according to the size, calculate the edge pixels radius according to the label size
+    @property
+    def marquee(self) -> bool:
+        return self._marquee
 
+    @marquee.setter
+    def marquee(self, activate) -> None:
+        self._marquee = activate
+
+    # TODO: Add rounded object according to the size, calculate the edge pixels radius according to the label size
     def construct_animation(self) -> None:
         self.setAlignment(
             QtCore.Qt.AlignmentFlag.AlignHCenter
@@ -58,19 +79,55 @@ class BlocksLabel(QtWidgets.QLabel):
         self.animation.setStartValue(QtGui.QColor("red"))
         self.animation.start()
 
-    def border_color(self, color):
-        self.setStyleSheet(f"border: 2px solid {color.name()};")
+    def update_text_metrics(self) -> None:
+        font_metrics = self.fontMetrics()
+        self.text_width = font_metrics.horizontalAdvance(self._text)
+        self.label_width = self.contentsRect().width()
+
+        if self.text_width > self.label_width:
+            self.start_scrolling()
+        else:
+            self.stop_scrolling()
+            self.scroll_pos = 0
+
+        self.repaint()
+
+    def start_scrolling(self) -> None:
+        if not self.timer.isActive():
+            self.scroll_pos = 0
+            self.timer.start(self.scroll_animation_speed)
+
+    def stop_scrolling(self) -> None:
+        if self.timer.isActive():
+            self.timer.stop()
+
+    def _scroll_text(self):
+        if self.paused:
+            return
+
+        p_to_m = int(
+            self.scroll_speed * (self.scroll_animation_speed / 1000.0)
+        )
+        self.scroll_pos -= p_to_m
+
+        if abs(self.scroll_pos) >= self.text_width:
+            self.scroll_pos = self.label_width
+
+        self.repaint()
 
     def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
         qp = QtWidgets.QStylePainter(self)
-        # qp.save()
         qp.setRenderHint(qp.RenderHint.Antialiasing, True)
         qp.setRenderHint(qp.RenderHint.SmoothPixmapTransform, True)
         qp.setRenderHint(qp.RenderHint.LosslessImageRendering, True)
+        # opt = QtWidgets.QStylePainter(self)
+
         _rect = self.rect()
         _style = self.style()
+
         if _style is None or _rect is None:
             return
+
         if self.icon_pixmap is not None:
             qp.setCompositionMode(
                 qp.CompositionMode.CompositionMode_SourceOver
@@ -129,6 +186,34 @@ class BlocksLabel(QtWidgets.QLabel):
             # else:
             #     qp.drawRect(self.rect())
         # qp.restore()
+
+        if self.text:
+            text_rect = self.contentsRect()
+            text_rect.translate(int(self.scroll_pos), 0)
+            text_option = QtGui.QTextOption(self.alignment())
+            text_option.setWrapMode(QtGui.QTextOption.WrapMode.NoWrap)
+            text_option.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft | QtCore.Qt.AlignmentFlag.AlignVCenter)
+            painter = QtGui.QPainter(self)
+            text_rect.setWidth(self.text_width)
+            painter.drawText(
+                text_rect.toRectF(),
+                self._text,
+                text_option,
+            )
+            if self.text_width > self.label_width and self.scroll_pos < 0:
+                second_text_rect = self.contentsRect()
+                second_text_rect.translate(
+                    int(
+                        self.scroll_pos
+                        + self.text_width
+                        + self.label_width / 2
+                    ),
+                    0,
+                )
+                painter.drawText(
+                    second_text_rect.toRectF(), self._text, text_option
+                )
+            painter.end()
 
     def setProperty(self, name: str, value: typing.Any) -> bool:
         if name == "icon_pixmap":
