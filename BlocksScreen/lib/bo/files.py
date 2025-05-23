7b846acb2983df226c8a1a60811a340acf29462a
HugoCLSC <hugo.santos.costa@gmail.com>
2025-05-23 16:32:59 +0100

    Refactor: Event filter for klipper ready, temporary fix for thumbnails

--- BlocksScreen/lib/bo/files.py
+++ BlocksScreen/lib/bo/files.py
@@ -1,39 +1,40 @@
 from __future__ import annotations
+
 import os
 
-from PyQt6 import QtCore, QtGui
-from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot
-import typing
-
+import events
 from events import ReceivedFileData
 from lib.moonrakerComm import MoonWebSocket
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 
 class Files(QtCore.QObject):
     # @ Signals
-    request_file_list = pyqtSignal(name="get_files_list")
-    request_file_metadata = pyqtSignal([str], name="get_file_metadata")
-    request_files_thumbnails = pyqtSignal([str], name="request_files_thumbnail")
-    request_file_download = pyqtSignal([str, str], name="file_download")
+    request_file_list = QtCore.pyqtSignal(name="get_files_list")
+    request_file_metadata = QtCore.pyqtSignal([str], name="get_file_metadata")
+    request_files_thumbnails = QtCore.pyqtSignal(
+        [str], name="request_files_thumbnail"
+    )
+    request_file_download = QtCore.pyqtSignal([str, str], name="file_download")
 
     def __init__(
         self,
-        parent: QObject,
+        parent: QtCore.QObject,
         ws: MoonWebSocket,
         update_interval: int = 5000,
     ) -> None:
         super(Files, self).__init__(parent)
         self.ws = ws
-
         self.gcode_path = os.path.expanduser("~/printer_data/gcodes")
-
         self.files: list = []
         self.files_metadata: dict = {}
-        # @ Connect signals
         self.request_file_list.connect(slot=self.ws.api.get_file_list)
         self.request_file_metadata.connect(slot=self.ws.api.get_gcode_metadata)
-        self.request_files_thumbnails.connect(slot=self.ws.api.get_gcode_thumbnail)
+        self.request_files_thumbnails.connect(
+            slot=self.ws.api.get_gcode_thumbnail
+        )
         self.request_file_download.connect(slot=self.ws.api.download_file)
+        QtWidgets.QApplication.instance().installEventFilter(self)
 
     @property
     def file_list(self):
@@ -43,23 +44,26 @@ class Files(QtCore.QObject):
         if "server.files.list" in method:
             self.files.clear()
             self.files = data
-            [self.request_file_metadata.emit(item["path"]) for item in self.files]
-
+            [
+                self.request_file_metadata.emit(item["path"])
+                for item in self.files
+            ]
         elif "server.files.metadata" in method:
             if data["filename"] in self.files_metadata.keys():
                 self.files_metadata.update({data["filename"]: data})
             else:
                 self.files_metadata[data["filename"]] = data
 
+        print(data)
+
     def event(self, a0: QtCore.QEvent) -> bool:
         if a0.type() == ReceivedFileData.type():
             if isinstance(a0, ReceivedFileData):
                 self.handle_message_received(a0.method, a0.data, a0.params)
-                # Handled
-                return True
+                return True  # Event Handled
         return super().event(a0)
 
-    @pyqtSlot(str, name="get_file_thumbnail")
+    @QtCore.pyqtSlot(str, name="get_file_thumbnail")
     def get_file_thumbnail(self, filename) -> QtGui.QImage | None:
         if self.files_metadata is None or filename is None:
             return None
@@ -68,10 +72,15 @@ class Files(QtCore.QObject):
             return None
 
         metadata = self.files_metadata[filename]
-
         if "thumbnails" in metadata:
-            _thumbnails = metadata["thumbnails"]
-            _thumb_relative_path = _thumbnails[1]["relative_path"]
+            _thumbnails = metadata.get("thumbnails")
+            if not _thumbnails:
+                return
+            if _thumbnails[2].get("relative_path"):
+                _thumb_relative_path = _thumbnails[2]["relative_path"]
+            else:
+                _thumb_relative_path = _thumbnails[1]["relative_path"]
+
             path = os.path.join(
                 os.path.dirname(os.path.join(self.gcode_path, filename)),
                 _thumb_relative_path,
@@ -82,8 +91,17 @@ class Files(QtCore.QObject):
                 return self.request_file_download[str, str].emit(
                     "~/printer_data/gcodes/.thumbs", filename
                 )
-
         return None
+
+    def eventFilter(self, a0: QtCore.QObject, a1: QtCore.QEvent) -> bool:
+        if a1.type() == events.KlippyDisconnected.type():
+            self.files_metadata.clear()
+            self.files.clear()
+            return False
+        elif a1.type() == events.KlippyReady.type():
+            self.request_file_list.emit()
+            return False
+        return super().eventFilter(a0, a1)
 
 
 # _image = None
