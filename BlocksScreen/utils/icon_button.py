a17c5e697180e5e462757cd305766953f4c655ec
HugoCLSC <hugo.santos.costa@gmail.com>
2025-05-23 17:09:30 +0100

    Added method for setting the icon pixmap

--- BlocksScreen/utils/icon_button.py
+++ BlocksScreen/utils/icon_button.py
@@ -21,6 +21,10 @@ class IconButton(QtWidgets.QPushButton):
     def text(self) -> str:
         return self._text
 
+    def setIconPixmap(self, pixmap: QtGui.QPixmap) -> None: 
+        self.icon_pixmap = pixmap
+        self.repaint()
+    
     def setText(self, text: str) -> None:
         self._text = text
         self.update()
