ea5c3ce720bad9f2056d4154c6b80e7a339ae693
HugoCLSC <hugo.santos.costa@gmail.com>
2025-08-05 15:10:10 +0100

    Add: get methods, with typing annotations

--- BlocksScreen/configfile.py
+++ BlocksScreen/configfile.py
@@ -6,18 +6,15 @@
 # https://github.com/Arksine/moonraker/blob/master/moonraker/confighelper.py
 #
 # This file may be distributed under the terms of GNU GPLv3 license
-
+from __future__ import annotations
 import configparser
 import enum
-import hashlib
-import io
-import logging
 import os
 import pathlib
 import re
 import typing
 
-from helper_methods import *  # check_filepath_permission, check_path_existence
+from helper_methods import check_file_on_path
 
 HOME_DIR = os.path.expanduser("~/")
 WORKING_DIR = os.getcwd()
@@ -26,13 +23,6 @@ FALLBACK_CONFIGFILE_PATH = pathlib.Path(WORKING_DIR)
 
 T = typing.TypeVar("T")
 
-
-class Sentinel(enum.Enum):
-    # Add Sentinel because `None` can
-    # actually be a valid value for an option
-    MISSING = object
-
-
 error = configparser.Error
 parsing_error = configparser.ParsingError
 no_option_error = configparser.NoOptionError
@@ -40,22 +30,88 @@ no_section_error = configparser.NoSectionError
 alien_error = configparser.ParsingError
 
 
+class Sentinel(enum.Enum):
+    # Add Sentinel because `None` can
+    # actually be a valid value for an option
+    MISSING = object
+
+
 class BlocksScreenConfig:
-    def __init__(self, configfile) -> None:
+    def __init__(
+        self, configfile: typing.Union[str, pathlib.Path], section: str
+    ) -> None:
         self.config = configparser.ConfigParser(
             allow_no_value=True, interpolation=None
         )
         self.configfile = pathlib.Path(configfile)
+        self.section = section
         self.load_config()
 
-    def __getitem__(self, key):
-        if not self.config.has_section(key):
-            raise no_section_error(f"No section named {key}")
-
-        return BlocksScreenConfig(self.configfile)
+    def __getitem__(self, key: str) -> BlocksScreenConfig:
+        return self.get_section(key)
 
     def __contains__(self, key):
         return key in self.config
+
+    def __repr__(self) -> str:
+        return f"Configuration for the {self.section} section"
+
+    # def __dir__(self) -> configparser.Iterable[str]:
+    #     return
+    def get_section(
+        self, section: str, fallback: typing.Optional[T] = None
+    ) -> BlocksScreenConfig:
+        if not self.config.has_section(section):
+            raise no_section_error(f"No section with name: {section}")
+        return BlocksScreenConfig(self.configfile, section)
+
+    def get_options(self) -> list:
+        return self.config.options(self.section)
+
+    def has_section(self, section: str) -> bool:
+        return bool(self.config.has_section(section))
+
+    def has_option(self, option: str) -> bool:
+        return bool(self.config.has_option(self.section, option))
+
+    def get(
+        self,
+        option: str,
+        parser: type = str,
+        default: typing.Union[Sentinel, str] = Sentinel.MISSING,
+    ) -> typing.Union[Sentinel, str]:
+        return parser(
+            self.config.get(
+                section=self.section, option=option, fallback=default
+            )
+        )
+
+    def getint(
+        self,
+        option: str,
+        default: typing.Union[Sentinel, int] = Sentinel.MISSING,
+    ) -> typing.Union[Sentinel, int]:
+        return self.config.getint(
+            section=self.section, option=option, fallback=default
+        )
+
+    def getfloat(
+        self,
+        option: str,
+        default: typing.Union[Sentinel, float] = Sentinel.MISSING,
+    ) -> typing.Union[Sentinel, float]:
+        return self.config.getfloat(
+            section=self.section, option=option, fallback=default
+        )
+
+    def getboolean(
+        self,
+        option: str,
+        default: typing.Union[Sentinel, bool] = Sentinel.MISSING,
+    ) -> typing.Union[Sentinel, bool]:
+        return self.config.getboolean(
+            section=self.section, option=option, fallback=default
+        )
 
     def load_config(self):
         try:
@@ -106,30 +162,6 @@ class BlocksScreenConfig:
                 f"Unexpected error while parsing configuration file: {e}"
             )
 
-    def get(
-        self,
-        option,
-        default: typing.Optional[typing.Union[Sentinel, T]] = Sentinel.MISSING,
-    ) -> T: ...
-
-    def getint(
-        self,
-        option,
-        default: typing.Optional[typing.Union[Sentinel, T]] = Sentinel.MISSING,
-    ) -> int: ...
-
-    def getfloat(
-        self,
-        option,
-        default: typing.Optional[typing.Union[Sentinel, T]] = Sentinel.MISSING,
-    ) -> float: ...
-
-    def getboolean(
-        self,
-        option,
-        default: typing.Optional[typing.Union[Sentinel, T]] = Sentinel.MISSING,
-    ) -> bool: ...
-
 
 def get_configparser() -> BlocksScreenConfig:
     wanted_target = os.path.join(DEFAULT_CONFIGFILE_PATH, "BlocksScreen.cfg")
@@ -139,8 +171,5 @@ def get_configparser() -> BlocksScreenConfig:
         if check_file_on_path(DEFAULT_CONFIGFILE_PATH, "BlocksScreen.cfg")
         else fallback
     )
-    return BlocksScreenConfig(configfile=configfile)
+    return BlocksScreenConfig(configfile=configfile, section="server")
 
-
-if __name__ == "__main__":
-    config = get_configparser()
