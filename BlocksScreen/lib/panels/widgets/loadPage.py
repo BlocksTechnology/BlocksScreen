507e5636122669951e77b4f0a59c132cec346b2a
Roberto Martins <robertomicael.martins@gmail.com>
2025-08-20 11:58:02 +0100

    ADDED: added the option to load an gif into loadpage widget instead of an circle animation

--- BlocksScreen/lib/panels/widgets/loadPage.py
+++ BlocksScreen/lib/panels/widgets/loadPage.py
@@ -1,14 +1,20 @@
+from ast import main
+
+import sys
+
 from PyQt6 import QtCore, QtGui, QtWidgets
+import enum
 
 
 class LoadScreen(QtWidgets.QDialog):
     def __init__(
-        self, parent: QtWidgets.QWidget, main_window: QtWidgets.QWidget
+        self,
+        parent: QtWidgets.QWidget,
+        gif: QtGui.QMovie = None,
     ) -> None:
         super().__init__(parent)
-        self.main_window = main_window
-        self.default_background_color = QtGui.QColor(0, 0, 0, 150)
 
+        self.gifs = gif
         self._angle = 0
         self._span_angle = 90.0
         self._is_span_growing = True
@@ -16,34 +22,59 @@ class LoadScreen(QtWidgets.QDialog):
         self.max_length = 150.0
         self.length_step = 2.5
 
+        self.setStyleSheet(
+            "background-image: url(:/background/media/1st_background.png);"
+        )
         self.setupUI()
 
-        self.setAttribute(
-            QtCore.Qt.WidgetAttribute.WA_TranslucentBackground, True
-        )
+        # Handle the GIF logic
+        if self.gifs is not None:
+            self.gifshow.setMovie(self.gifs)
+            self.gifs.start()
 
-        # Safely try to set the 'always on top' flag for better compatibility
         self.setWindowFlags(
             QtCore.Qt.WindowType.Popup
             | QtCore.Qt.WindowType.FramelessWindowHint
         )
 
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Policy.MinimumExpanding,
-            QtWidgets.QSizePolicy.Policy.MinimumExpanding,
-        )
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
-        self.setSizePolicy(sizePolicy)
-
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self._update_animation)
-        self.timer.start(16)
+
+        # Only start the animation timer if no GIF is provided
+        if self.gifs is None:
+            self.timer.start(16)
+
+        self.repaint()
+
+    def set_status_message(self, message: str) -> None:
+        self.label.setText(message)
+
+    def geometry_calc(self) -> None:
+        # REFACTOR: find another way to get mainwindow geometry , this version consumes too much ram
+        app_instance = QtWidgets.QApplication.instance()
+        main_window = app_instance.activeWindow() if app_instance else None
+        if main_window is None and app_instance:
+            for widget in app_instance.allWidgets():
+                if isinstance(widget, QtWidgets.QMainWindow):
+                    main_window = widget
+        x = main_window.geometry().x()
+        y = main_window.geometry().y()
+        width = main_window.width()
+        height = main_window.height()
+
+        self.setGeometry(x, y, width, height)
+
+    def close(self) -> bool:
+        self.timer.stop()
+        self.label.setText("Loading...")
+        self._angle = 0
+        # Stop the GIF animation if it was started
+        if self.gifs is not None:
+            self.gifs.stop()
+        return super().close()
 
     def _update_animation(self) -> None:
         self._angle = (self._angle + 5) % 360
-
         if self._is_span_growing:
             self._span_angle += self.length_step
             if self._span_angle >= self.max_length:
@@ -56,75 +87,88 @@ class LoadScreen(QtWidgets.QDialog):
                 self._is_span_growing = True
         self.update()
 
-    def set_status_message(self, message: str) -> None:
-        self.label.setText(message)
-
     def sizeHint(self) -> QtCore.QSize:
-        # Use the explicitly passed main_window to get geometry
-        parent_rect = self.main_window.geometry()
-
-        popup_width = int(parent_rect.width() * 0.90)
-        popup_height = int(parent_rect.height() * 0.90)
-
+        popup_width = int(self.geometry().width())
+        popup_height = int(self.geometry().height())
         # Centering logic
-        popup_x = parent_rect.x() + (parent_rect.width() - popup_width) // 2
-        popup_y = parent_rect.y() + (parent_rect.height() - popup_height) // 2
 
+        popup_x = self.x()
+        popup_y = self.y() + (self.height() - popup_height) // 2
         self.move(popup_x, popup_y)
         self.setFixedSize(popup_width, popup_height)
         self.setMinimumSize(popup_width, popup_height)
         return super().sizeHint()
 
     def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
-        self.close()
+        return
 
     def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
         painter = QtGui.QPainter(self)
-        painter.setRenderHint(painter.RenderHint.Antialiasing, True)
-        painter.setRenderHint(painter.RenderHint.LosslessImageRendering, True)
-        painter.setRenderHint(painter.RenderHint.SmoothPixmapTransform, True)
-        painter.setRenderHint(painter.RenderHint.TextAntialiasing, True)
+        # loading circle draw
+        if self.gifs is None:
+            painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing, True)
+            painter.setRenderHint(
+                QtGui.QPainter.RenderHint.LosslessImageRendering, True
+            )
+            painter.setRenderHint(
+                QtGui.QPainter.RenderHint.SmoothPixmapTransform, True
+            )
+            painter.setRenderHint(
+                QtGui.QPainter.RenderHint.TextAntialiasing, True
+            )
+            pen = QtGui.QPen()
+            pen.setWidth(8)
+            pen.setColor(QtGui.QColor("#ffffff"))
+            pen.setCapStyle(QtCore.Qt.PenCapStyle.RoundCap)
+            painter.setPen(pen)
 
-        popup_path = QtGui.QPainterPath()
-        popup_path.addRoundedRect(self.rect().toRectF(), 10, 10)
-        _background_color = self.default_background_color
-        painter.setBrush(_background_color)
-        painter.setPen(QtCore.Qt.PenStyle.NoPen)
-        painter.fillPath(popup_path, painter.brush())
+            center_x = self.width() // 2
+            center_y = int(self.height() * 0.4)
+            arc_size = 150
 
-        pen = QtGui.QPen()
-        pen.setWidth(8)
-        pen.setColor(QtGui.QColor("#ffffff"))
-        pen.setCapStyle(QtCore.Qt.PenCapStyle.RoundCap)
-        painter.setPen(pen)
+            painter.translate(center_x, center_y)
+            painter.rotate(self._angle)
 
-        center_x = self.width() // 2
-        center_y = int(self.height() * 0.4)
-        arc_size = 150
-
-        painter.translate(center_x, center_y)
-        painter.rotate(self._angle)
-
-        arc_rect = QtCore.QRectF(
-            -arc_size / 2, -arc_size / 2, arc_size, arc_size
-        )
-        start_angle = 0
-        span_angle = int(self._span_angle * 16)
-        painter.drawArc(arc_rect, start_angle, span_angle)
+            arc_rect = QtCore.QRectF(
+                -arc_size / 2, -arc_size / 2, arc_size, arc_size
+            )
+            span_angle = int(self._span_angle * 16)
+            painter.drawArc(arc_rect, 0, span_angle)
 
     def resizeEvent(self, event: QtGui.QResizeEvent) -> None:
         super().resizeEvent(event)
-        label_width = 500
+
+        label_width = self.width()
         label_height = 100
         label_x = (self.width() - label_width) // 2
         label_y = int(self.height() * 0.65)
+
+        margin = 20
+        # Center the GIF
+        gifshow_width = self.width() - margin * 2
+        gifshow_height = self.height() - (self.height() - label_y) - margin
+
+        self.gifshow.setGeometry(margin, margin, gifshow_width, gifshow_height)
+
         self.label.setGeometry(label_x, label_y, label_width, label_height)
 
+    def show(self) -> None:
+        self.geometry_calc()
+        # Start the animation timer only if no GIF is present
+        if self.gifs is None:
+            self.timer.start()
+        self.repaint()
+        return super().show()
+
     def setupUI(self) -> None:
-        self.setObjectName("Popup")
+        self.gifshow = QtWidgets.QLabel("", self)
+        self.gifshow.setObjectName("gifshow")
+        self.gifshow.setStyleSheet("background: transparent;")
+        self.gifshow.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+
         self.label = QtWidgets.QLabel("Test", self)
         font = QtGui.QFont()
-        font.setPointSize(15)
+        font.setPointSize(20)
         self.label.setFont(font)
-        self.label.setStyleSheet("color: #ffffff;")
+        self.label.setStyleSheet("color: #ffffff; background: transparent;")
         self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
