83b8e2810deb09216746bb5b9275929b9aaf0f97
Roberto Martins <robertomicael.martins@gmail.com>
2025-10-07 14:50:41 +0100

    Blocks_button notification (#49)
    * add: added notification circle to buttons and tab icons , added blocks_button grey when disabled
    
    * Refactor: centered blocks_button text

--- BlocksScreen/lib/utils/blocks_button.py
+++ BlocksScreen/lib/utils/blocks_button.py
@@ -1,23 +1,23 @@
 import typing
+import enum
 from PyQt6 import QtCore, QtGui, QtWidgets
 
 
+class ButtonColors(enum.Enum):
+    NORMAL_BG = (223, 223, 223)
+    PRESSED_BG = (169, 169, 169)
+    DISABLED_BG = (169, 169, 169)
+    TEXT_COLOR = (0, 0, 0)
+    DISABLED_TEXT_COLOR = (102, 102, 102)
+    NOTIFICATION_DOT = (226, 31, 31)
+
+
 class BlocksCustomButton(QtWidgets.QPushButton):
-    """Custom Blocks QPushButton
-        Rounded button with a hole on the left side where an icon can be inserted
-
-    Args:
-        parent (QWidget): Parent of the button
-    """
-
     def __init__(
         self,
         parent: QtWidgets.QWidget = None,
     ) -> None:
-        if parent:
-            super(BlocksCustomButton, self).__init__(parent)
-        else:
-            super(BlocksCustomButton, self).__init__()
+        super().__init__(parent)
 
         self.icon_pixmap: QtGui.QPixmap = QtGui.QPixmap()
         self._icon_rect: QtCore.QRectF = QtCore.QRectF()
@@ -25,8 +25,14 @@ class BlocksCustomButton(QtWidgets.QPushButton):
         self.button_ellipse = None
         self._text: str = ""
         self._name: str = ""
-        self.text_color: QtGui.QColor = QtGui.QColor(0, 0, 0)
+        self.text_color: QtGui.QColor = QtGui.QColor(*ButtonColors.TEXT_COLOR.value)
+        self._show_notification: bool = False
         self.setAttribute(QtCore.Qt.WidgetAttribute.WA_AcceptTouchEvents, True)
+
+    def setShowNotification(self, show: bool) -> None:
+        if self._show_notification != show:
+            self._show_notification = show
+            self.update()
 
     @property
     def name(self):
@@ -42,7 +48,7 @@ class BlocksCustomButton(QtWidgets.QPushButton):
 
     def setText(self, text: str) -> None:
         self._text = text
-        self.update()  # Force button update
+        self.update()
         return
 
     def setPixmap(self, pixmap: QtGui.QPixmap) -> None:
@@ -50,11 +56,13 @@ class BlocksCustomButton(QtWidgets.QPushButton):
         self.repaint()
 
     def mousePressEvent(self, e: QtGui.QMouseEvent) -> None:
-        """Overwritten method so 'normal' buttons can only be
-        pressed in the visible area
-        """
-        if not self.button_background:
-            if self.button_background.contains(e.pos()):  # type:ignore
+        if not self.isEnabled():
+            e.ignore()
+            return
+            
+        if self.button_background is not None:
+            pos_f = QtCore.QPointF(e.pos())
+            if self.button_background.contains(pos_f):
                 super().mousePressEvent(e)
                 return
             else:
@@ -79,25 +87,20 @@ class BlocksCustomButton(QtWidgets.QPushButton):
         margin = _style.pixelMetric(
             _style.PixelMetric.PM_ButtonMargin, opt, self
         )
-        bg_color = (
-            QtGui.QColor(164, 164, 164)
-            if self.isDown()
-            else QtGui.QColor(223, 223, 223)
-        )
-
-        # bg_color = (
-        #     QtGui.QColor(164, 164, 164)
-        #     if self.isDown()
-        #     else QtGui.QColor(
-        #         *(
-        #             map(
-        #                 lambda component: int(component + component * 0.365),
-        #                 (164,164,164),
-        #             )
-        #         )
-        #     )
-        # )
-
+        
+        # Determine background and text colors based on state
+        if not self.isEnabled():
+            bg_color_tuple = ButtonColors.DISABLED_BG.value
+            current_text_color = QtGui.QColor(*ButtonColors.DISABLED_TEXT_COLOR.value)
+        elif self.isDown():
+            bg_color_tuple = ButtonColors.PRESSED_BG.value
+            current_text_color = self.text_color
+        else:
+            bg_color_tuple = ButtonColors.NORMAL_BG.value
+            current_text_color = self.text_color
+            
+        bg_color = QtGui.QColor(*bg_color_tuple)
+        
         path = QtGui.QPainterPath()
         xRadius = self.rect().toRectF().normalized().height() / 2.0
         yRadius = self.rect().toRectF().normalized().height() / 2.0
@@ -126,7 +129,6 @@ class BlocksCustomButton(QtWidgets.QPushButton):
         painter.setBrush(bg_color)
         painter.fillPath(self.button_background, bg_color)
 
-        # Draw Icon
         _parent_rect = self.button_ellipse.toRect()
         _icon_rect = QtCore.QRectF(
             _parent_rect.left() * 2.8,
@@ -141,46 +143,70 @@ class BlocksCustomButton(QtWidgets.QPushButton):
                 QtCore.Qt.TransformationMode.SmoothTransformation,
             )
 
-            # Calculate the actual QRect for the scaled pixmap (centering it if needed)
             scaled_width = _icon_scaled.width()
             scaled_height = _icon_scaled.height()
-            adjusted_x = (_icon_rect.width() - scaled_width) // 2.0
-            adjusted_y = (_icon_rect.height() - scaled_height) // 2.0
+            adjusted_x = (_icon_rect.width() - scaled_width) / 2.0
+            adjusted_y = (_icon_rect.height() - scaled_height) / 2.0
             adjusted_icon_rect = QtCore.QRectF(
                 _icon_rect.x() + adjusted_x,
                 _icon_rect.y() + adjusted_y,
                 scaled_width,
                 scaled_height,
             )
+            
+            tinted_icon_pixmap = QtGui.QPixmap(_icon_scaled.size())
+            tinted_icon_pixmap.fill(QtCore.Qt.GlobalColor.transparent)
+
+            icon_painter = QtGui.QPainter(tinted_icon_pixmap)
+            icon_painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing, True)
+            icon_painter.setRenderHint(QtGui.QPainter.RenderHint.SmoothPixmapTransform, True)
+            icon_painter.drawPixmap(0, 0, _icon_scaled)
+            icon_painter.setCompositionMode(QtGui.QPainter.CompositionMode.CompositionMode_SourceIn)
+            icon_painter.setBrush(bg_color)
+            icon_painter.setPen(QtCore.Qt.PenStyle.NoPen)
+            icon_painter.drawRect(tinted_icon_pixmap.rect())
+            icon_painter.end()
+
+            destination_point = adjusted_icon_rect.toRect().topLeft()
 
             painter.drawPixmap(
-                adjusted_icon_rect,  # Target area (center adjusted)
-                _icon_scaled,  # Scaled pixmap
-                _icon_scaled.rect().toRectF(),  # Entire source (scaled) pixmap
+                destination_point,
+                tinted_icon_pixmap
             )
 
         if self.text():
             _start_text_position = int(self.button_ellipse.width())
             _text_rect = _rect
-            _text_rect.setLeft(_start_text_position + margin)
             _text_rect.setWidth(
                 self.width() - int(self.button_ellipse.width())
             )
+            _text_rect.setLeft(int(self.button_ellipse.width()))
             _pen = painter.pen()
             _pen.setStyle(QtCore.Qt.PenStyle.SolidLine)
             _pen.setWidth(1)
-            _pen.setColor(QtGui.QColor(self.text_color))
+            
+            _pen.setColor(current_text_color)
             painter.setPen(_pen)
 
             painter.drawText(
                 _text_rect,
                 QtCore.Qt.TextFlag.TextShowMnemonic
-                # | QtCore.Qt.AlignmentFlag.AlignHCenter
-                | QtCore.Qt.AlignmentFlag.AlignLeft
-                | QtCore.Qt.AlignmentFlag.AlignVCenter,
+                | QtCore.Qt.AlignmentFlag.AlignCenter,
                 str(self.text()),
             )
             painter.setPen(QtCore.Qt.PenStyle.NoPen)
+
+        if self._show_notification:
+
+            dot_diameter = self.height() * 0.4
+            dot_x = self.width() - dot_diameter
+            
+            notification_color = QtGui.QColor(*ButtonColors.NOTIFICATION_DOT.value)
+            painter.setBrush(notification_color)
+            painter.setPen(QtCore.Qt.PenStyle.NoPen)
+            
+            dot_rect = QtCore.QRectF(dot_x,0, dot_diameter, dot_diameter)
+            painter.drawEllipse(dot_rect)
 
     def setProperty(self, name: str, value: typing.Any):
         if name == "icon_pixmap":
@@ -189,19 +215,9 @@ class BlocksCustomButton(QtWidgets.QPushButton):
             self._name = name
         elif name == "text_color":
             self.text_color = QtGui.QColor(value)
-        # return super().setProperty(name, value)
+            self.update()
 
-    def handleTouchBegin(self, e: QtCore.QEvent):
-        ...
-        # if not self.button_background:
-        #     if self.button_background.contains(e.pos()):  # type: ignore
-        #         # super().mousePressEvent(e)
-        #         self.mousePressEvent(e)  # type: ignore
-        #         return
-        #     else:
-        #         e.ignore()
-        #         return
-
+    def handleTouchBegin(self, e: QtCore.QEvent):...
     def handleTouchUpdate(self, e: QtCore.QEvent): ...
     def handleTouchEnd(self, e: QtCore.QEvent): ...
     def handleTouchCancel(self, e: QtCore.QEvent): ...
