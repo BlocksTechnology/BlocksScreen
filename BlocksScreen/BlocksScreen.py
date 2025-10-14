24ce976b46dc746e6c0efdc6d264e529f13a7b75
HugoCLSC <hugo.santos.costa@gmail.com>
2025-10-14 16:10:08 +0100

    Refactor

--- BlocksScreen/BlocksScreen.py
+++ BlocksScreen/BlocksScreen.py
@@ -1,9 +1,7 @@
-
 import logging
 import sys
 import typing
 
-import helper_methods as helper_methods
 import logger
 from lib.panels.mainWindow import MainWindow
 from PyQt6 import QtCore, QtGui, QtWidgets
@@ -28,36 +26,26 @@ def setup_working_dir(): ...
 
 
 def setup_app_loggers():
-    ql = logger.create_logger(
-        name="logs/BlocksScreen.log", level=logging.DEBUG
-    )
+    ql = logger.create_logger(name="logs/BlocksScreen.log", level=logging.DEBUG)
     _logger = logging.getLogger(name="logs/BlocksScreen.log")
     _logger.info("============ BlocksScreen Initializing ============")
 
 
 def show_splash(window: typing.Optional[QtWidgets.QWidget] = None):
-    logo = QtGui.QPixmap(
-        "BlocksScreen/BlocksScreen/lib/ui/resources/logoblocks.png"
-    )
+    logo = QtGui.QPixmap("BlocksScreen/BlocksScreen/lib/ui/resources/logoblocks.png")
     splash = QtWidgets.QSplashScreen(pixmap=logo)
     splash.setGeometry(QtCore.QRect(0, 0, 400, 200))
     if window is not None and isinstance(window, QtWidgets.QWidget):
         splash.finish(window)
 
 
-def run():
+if __name__ == "__main__":
     setup_app_loggers()
     BlocksScreen = QtWidgets.QApplication([])
     BlocksScreen.setApplicationName("BlocksScreen")
     BlocksScreen.setApplicationDisplayName("BlocksScreen")
     BlocksScreen.setDesktopFileName("BlocksScreen")
-    
     main_window = MainWindow()
-
     BlocksScreen.processEvents()
     main_window.show()
     sys.exit(BlocksScreen.exec())
-
-
-if __name__ == "__main__":
-    run()
