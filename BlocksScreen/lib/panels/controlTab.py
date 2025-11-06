f75971e01057fd6f7f9e21728606f98aceebf207
Roberto Martins <robertomicael.martins@gmail.com>
2025-11-06 17:56:53 +0000

    Refactor: moved gcode response to innit (#93)
    Co-authored-by: Hugo Costa <hugo.santos.costa@gmail.com>

--- BlocksScreen/lib/panels/controlTab.py
+++ BlocksScreen/lib/panels/controlTab.py
@@ -267,6 +267,7 @@ class ControlTab(QtWidgets.QStackedWidget):
         self.ws.klippy_state_signal.connect(self.on_klippy_status)
         self.ws.klippy_state_signal.connect(self.probe_helper_page.on_klippy_status)
         self.printer.on_printcore_update.connect(self.handle_printcoreupdate)
+        self.printer.gcode_response.connect(self._handle_gcode_response)
 
         self.panel.cp_printer_settings_btn.hide()
 
@@ -290,8 +291,6 @@ class ControlTab(QtWidgets.QStackedWidget):
             self.loadpage.set_status_message("Cleaning print core")
 
         
-
-        self.printer.gcode_response.connect(self._handle_gcode_response)
 
 
 
