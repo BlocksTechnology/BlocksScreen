b5463ce4d0573706e036d374d055ffe5257d2374
HugoCLSC <hugo.santos.costa@gmail.com>
2025-12-05 19:39:33 +0000

    Added docstrings, deleted commented code

--- BlocksScreen/lib/machine.py
+++ BlocksScreen/lib/machine.py
@@ -1,3 +1,6 @@
+#
+# Machine manager
+#
 import logging
 import subprocess
 import typing
@@ -11,34 +14,40 @@ class MachineControl(QObject):
     def __init__(self, parent: typing.Optional["QObject"]) -> None:
         super(MachineControl, self).__init__(parent)
         self.setObjectName("MachineControl")
-    
+
     @pyqtSlot(name="machine_restart")
     def machine_restart(self):
+        """Reboot machine"""
         return self._run_command("sudo reboot now")
 
     @pyqtSlot(name="machine_shutdown")
     def machine_shutdown(self):
+        """Shutdown machine"""
         return self._run_command("sudo shutdown now")
 
     @pyqtSlot(name="restart_klipper_service")
     def restart_klipper_service(self):
-        # self.service_restart.emit("restart-klipper-service")
+        """Restart klipper service"""
         return self._run_command("sudo systemctl stop klipper.service")
-    
+
     @pyqtSlot(name="restart_moonraker_service")
     def restart_moonraker_service(self):
-        # self.service_restart.emit("restart-moonraker-service")
+        """Restart moonraker service"""
         return self._run_command("sudo systemctl restart moonraker.service")
 
-    def restart_bo_service(self):
-        # TODO: Restart Blocks Screen service, implement it later on
-        pass
-
     def check_service_state(self, service_name: str):
+        """Check service status
+
+        Args:
+            service_name (str): service name
+
+        Returns:
+            _type_: output of the command `systemctl is-active <service name>`
+        """
         if service_name is None:
             return None
         return self._run_command(f"systemctl is-active {service_name}")
-    
+
     def _run_command(self, command):
         """Runs a shell command.
 
@@ -50,18 +59,10 @@ class MachineControl(QObject):
 
         """
         try:
-            # REVIEW: Safe way to run bash commands 
-            # * Old way, it didn't let me use grep commands or use | on the command
-            # cmd = shlex.split(command,posix=False)
-            # exec = cmd[0]
-            # exec_options = cmd[1:]
-            # output = subprocess.run(
-            #     ([exec] + exec_options), capture_output=True)
-            # TEST: is this safe to use like this, or is it susceptible to attacks and stuff
             p = subprocess.Popen(
                 command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
             )
-            output, e = p.communicate()
+            output, _ = p.communicate()
             return output
         except subprocess.SubprocessError:
             logging.error("Error running commas : %s", command)
