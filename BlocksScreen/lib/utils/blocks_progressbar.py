ed05aa20f6bd2e4e4af918000e1cf5d90adf2b48
HugoCLSC <hugo.santos.costa@gmail.com>
2025-12-18 12:43:16 +0000

    Final progress bar refactor, send signal on thumbnail click
    Refactored class method positions to add some structure to the class.
    Privatised some class variables, should add @property decorator with
    getter and setter for these variables since they are actually properties.
    Captured MousePressEvent of the widget and filtered the events only to
    those inside the inner rect of the widget, when triggered, send an event
    `thumbnail_clicked` to signal that the thumbnail was actually clicked.

--- BlocksScreen/lib/utils/blocks_progressbar.py
+++ BlocksScreen/lib/utils/blocks_progressbar.py
@@ -1,27 +1,43 @@
+import typing
 from PyQt6 import QtWidgets, QtGui, QtCore
 
 
 class CustomProgressBar(QtWidgets.QProgressBar):
+    """Custom circular progress bar for tracking print jobs
+
+    Args:
+        QtWidgets (QtWidget): Parent widget
+
+    Raises:
+        ValueError: Thrown when setting progress is not between 0.0 and 1.0
+        ValueError: Thrown when setting bar color is not between 0 and 255.
+
+    """
+
+    thumbnail_clicked: typing.ClassVar[QtCore.pyqtSignal] = QtCore.pyqtSignal(
+        name="thumbnail-clicked"
+    )
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.progress_value = 0
-        self.pen_width = 20
-        self.padding = 50
+        self._pen_width = 20
+        self._padding = 50
         self._pixmap: QtGui.QPixmap = QtGui.QPixmap()
         self._pixmap_cached: QtGui.QPixmap = QtGui.QPixmap()
         self._pixmap_dirty: bool = True
-        self.bar_color = QtGui.QColor(223, 223, 223)
+        self._bar_color = QtGui.QColor(223, 223, 223)
         self.setMinimumSize(100, 100)
         self._inner_rect: QtCore.QRectF = QtCore.QRectF()
 
     def set_padding(self, value) -> None:
         """Set widget padding"""
-        self.padding = value
+        self._padding = value
         self.update()
 
     def set_pen_width(self, value) -> None:
         """Set widget text pen width"""
-        self.pen_width = value
+        self._pen_width = value
         self.update()
 
     def set_inner_pixmap(self, pixmap: QtGui.QPixmap) -> None:
@@ -29,7 +45,6 @@ class CustomProgressBar(QtWidgets.QProgressBar):
         circumference.
         """
         self._pixmap = pixmap
-        self._pixmap_dirty = True
         self.update()
 
     def resizeEvent(self, a0) -> None:
@@ -38,14 +53,12 @@ class CustomProgressBar(QtWidgets.QProgressBar):
         Currently rescales the set pixmap so it has the optimal
         size.
         """
-        self._pixmap_dirty = True
         self._inner_rect = self._calculate_inner_geometry()
-        if not self._pixmap.isNull():
-            self._pixmap_cached = self._pixmap.scaled(
-                self._inner_rect.size().toSize(),
-                QtCore.Qt.AspectRatioMode.KeepAspectRatio,
-                QtCore.Qt.TransformationMode.SmoothTransformation,
-            )
+        self._pixmap_cached = self._pixmap.scaled(
+            self._inner_rect.size().toSize(),
+            QtCore.Qt.AspectRatioMode.KeepAspectRatio,
+            QtCore.Qt.TransformationMode.SmoothTransformation,
+        )
         self.update()
         return super().resizeEvent(a0)
 
@@ -54,39 +67,57 @@ class CustomProgressBar(QtWidgets.QProgressBar):
         self._inner_rect = self._calculate_inner_geometry()
         return QtCore.QSize(100, 100)
 
+    def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
+        """Re-implemented method, check if thumbnail was clicked,
+        filter clicks inside inner section of the widget,
+        if a mouse event happens there we know that the thumbnail
+        was pressed.
+        """
+        if self._inner_rect.contains(a0.pos().x(), a0.pos().y()):
+            self.thumbnail_clicked.emit()
+        return super().mousePressEvent(a0)
+
     def minimumSizeHint(self) -> QtCore.QSize:
         """Re-implemented method, minimum widget size"""
         self._inner_rect = self._calculate_inner_geometry()
         return QtCore.QSize(100, 100)
 
+    def setValue(self, value):
+        """Set progress value"""
+        value *= 100
+        if 0 <= value <= 101:
+            self.progress_value = value
+            self.update()
+        else:
+            raise ValueError("Progress must be between 0.0 and 1.0.")
+
+    def set_bar_color(self, red, green, blue):
+        """Set widget bar color"""
+        if 0 <= red <= 255 and 0 <= green <= 255 and 0 <= blue <= 255:
+            self._bar_color = QtGui.QColor(red, green, blue)
+            self.update()
+        else:
+            raise ValueError("Color values must be between 0 and 255.")
+
     def _calculate_inner_geometry(self) -> QtCore.QRectF:
-        size = min(self.width(), self.height()) - (self.padding * 1.3)
+        size = min(self.width(), self.height()) - (self._padding * 1.3)
         x = (self.width() - size) // 2
         y = (self.height() - size) // 2
         return QtCore.QRectF(
-            x + self.pen_width // 2,
-            y + self.pen_width // 2,
-            size - self.pen_width,
-            size - self.pen_width,
+            x + self._pen_width // 2,
+            y + self._pen_width // 2,
+            size - self._pen_width,
+            size - self._pen_width,
         )
 
-    def paintEvent(self, event) -> None:
-        """Re-implemented method, paint widget"""
-        painter = QtGui.QPainter(self)
-        painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing)
-        self._inner_rect: QtCore.QRectF = self._draw_circular_bar(painter)
-        # if self._pixmap_dirty:
-        self._draw_cached_pixmap(painter, self._inner_rect)
-        painter.end()
-
     def _draw_cached_pixmap(
-        self, painter: QtGui.QPainter, inner_rect: QtCore.QRectF
+        self, painter: QtGui.QPainter, pixmap: QtGui.QPixmap, inner_rect: QtCore.QRectF
     ) -> None:
-        if self._pixmap_cached.isNull():
+        """Internal method draw already scaled pixmap on the widget inner section"""
+        if pixmap.isNull():
             return
-        # inner_rect = self._calculate_inner_geometry()
-        scaled_width = self._pixmap_cached.width()
-        scaled_height = self._pixmap_cached.height()
+        scaled_width = pixmap.width()
+        scaled_height = pixmap.height()
         adjusted_x = (inner_rect.width() - scaled_width) // 2.0
         adjusted_y = (inner_rect.height() - scaled_height) // 2.0
         adjusted_icon = QtCore.QRectF(
@@ -95,38 +126,35 @@ class CustomProgressBar(QtWidgets.QProgressBar):
             scaled_width,
             scaled_height,
         )
-        painter.drawPixmap(
-            adjusted_icon, self._pixmap_cached, self._pixmap_cached.rect().toRectF()
-        )
-        self._pixmap_dirty = False
+        painter.drawPixmap(adjusted_icon, pixmap, pixmap.rect().toRectF())
 
     def _draw_circular_bar(
         self,
         painter: QtGui.QPainter,
     ) -> None:
-        size = min(self.width(), self.height()) - (self.padding * 1.3)
+        size = min(self.width(), self.height()) - (self._padding * 1.3)
         x = (self.width() - size) / 2
         y = (self.height() - size) / 2
         arc_rect = QtCore.QRectF(x, y, size, size)
-        arc1_start = 236 * 16
-        arc1_span = -290 * 16
+        arc_start = 236 * 16
+        arc_span = -290 * 16
         bg_pen = QtGui.QPen(QtGui.QColor(20, 20, 20))
-        bg_pen.setWidth(self.pen_width)
+        bg_pen.setWidth(self._pen_width)
         bg_pen.setCapStyle(QtCore.Qt.PenCapStyle.RoundCap)
         painter.setPen(bg_pen)
-        painter.drawArc(arc_rect, arc1_start, arc1_span)
+        painter.drawArc(arc_rect, arc_start, arc_span)
         if self.progress_value is not None:
             gradient = QtGui.QConicalGradient(arc_rect.center(), -90)
-            gradient.setColorAt(0.0, self.bar_color)
+            gradient.setColorAt(0.0, self._bar_color)
             gradient.setColorAt(1.0, QtGui.QColor(100, 100, 100))
             progress_pen = QtGui.QPen()
-            progress_pen.setWidth(self.pen_width)
+            progress_pen.setWidth(self._pen_width)
             progress_pen.setCapStyle(QtCore.Qt.PenCapStyle.RoundCap)
             progress_pen.setBrush(QtGui.QBrush(gradient))
             painter.setPen(progress_pen)
-            # scale only over arc1’s span
-            progress_span = int(arc1_span * self.progress_value / 100)
-            painter.drawArc(arc_rect, arc1_start, progress_span)
+            # scale only over arc span
+            progress_span = int(arc_span * self.progress_value / 100)
+            painter.drawArc(arc_rect, arc_start, progress_span)
         progress_text = f"{int(self.progress_value)}%"
         painter.setPen(QtGui.QPen(QtGui.QColor(0, 0, 0)))
         font = painter.font()
@@ -141,19 +169,10 @@ class CustomProgressBar(QtWidgets.QProgressBar):
         )
         painter.drawText(text_rect, QtCore.Qt.AlignmentFlag.AlignCenter, progress_text)
 
-    def setValue(self, value):
-        """Set value"""
-        value *= 100
-        if 0 <= value <= 101:
-            self.progress_value = value
-            self.update()
-        else:
-            raise ValueError("Progress must be between 0.0 and 1.0.")
-
-    def set_bar_color(self, red, green, blue):
-        """Set bar color"""
-        if 0 <= red <= 255 and 0 <= green <= 255 and 0 <= blue <= 255:
-            self.bar_color = QtGui.QColor(red, green, blue)
-            self.update()
-        else:
-            raise ValueError("Color values must be between 0 and 255.")
+    def paintEvent(self, _) -> None:
+        """Re-implemented method, paint widget"""
+        painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing)
+        self._draw_circular_bar(painter)
+        self._draw_cached_pixmap(painter, self._pixmap_cached, self._inner_rect)
+        painter.end()
