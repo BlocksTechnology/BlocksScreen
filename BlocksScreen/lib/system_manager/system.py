07313cd294adeb04fd68099f84e4947c04a88bb5
HugoCLSC <hugo.santos.costa@gmail.com>
2026-01-27 10:22:33 +0000

    command helper for running commands

--- BlocksScreen/lib/system_manager/system.py
+++ BlocksScreen/lib/system_manager/system.py
@@ -2,6 +2,7 @@
 # System management
 #
 import logging
+import os
 import shlex
 import subprocess
 import typing
@@ -14,7 +15,7 @@ class SystemManager(QtCore.QObject):
         str, name="service-restart"
     )
 
-    def __init__(self, parent: typing.Optional["QtCore.QObject"]) -> None:
+    def __init__(self, parent: QtCore.QObject | None) -> None:
         if parent:
             super().__init__(parent)
         else:
@@ -23,47 +24,44 @@ class SystemManager(QtCore.QObject):
     @QtCore.pyqtSlot(name="host-restart")
     def host_restart(self):
         """Reboot host controller"""
-        return self._run_command("sudo reboot now")
+        return self._command_helper("reboot now")
 
     @QtCore.pyqtSlot(name="host-shutdown")
     def host_shutdown(self):
         """Shutdown host controller"""
-        return self._run_command("sudo shutdown now")
+        return self._command_helper("shutdown now")
 
-    @QtCore.pyqtSlot(name="klipper-service-restart")
+    @QtCore.pyqtSlot(name="klipper-service-restart")  # ignore
     def klipper_service_restart(self):
         """Restart klipper service"""
-        return self._run_command("sudo systemctl restart klipper.service")
+        return self._command_helper("systemctl --user restart klipper.service")
 
     @QtCore.pyqtSlot(name="moonraker-service-restart")
     def moonraker_service_restart(self):
         """Restart moonraker service"""
-        return self._run_command("sudo systemctl restart moonraker.service")
+        return self._command_helper("systemctl --user restart moonraker.service")
 
     def check_service_state(self, service_name: str):
         """Check service status
-
         Args:
             service_name (str): service name
-
         Return:
-            typing.Literal : The output of the command `systemctl is-active {service_name}`
+            typing.Literal : The output of the command `systemctl is-active`
         """
 
         if service_name:
             return "invalid"
-        return self._run_command("systemctl is-active %s", service_name)
+        return self._command_helper(
+            f"systemctl --user is-active {shlex.quote(service_name)}"
+        )  # TODO: JUST PARSE THE CORRECT OUTPUT OF THE COMMAND
 
     def _command_helper(self, command: str) -> str:
         """Run shell command
-
         Args:
-            str: Command to be executed
-
+            str: Command execute
         Returns:
-            str: Complete command output
+            str: Command output
         """
-
         try:
             # Split command into a list of strings
             cmd = shlex.split(command)
@@ -88,3 +86,44 @@ class SystemManager(QtCore.QObject):
             FileNotFoundError,
         ):
             logging.error("Caught exception, failed to run command %s", command)
+
+
+class CommandHelper:
+    def __init__(self) -> None:
+        self.stdout: int = subprocess.PIPE
+        self.stderr: int = subprocess.PIPE
+        self.proc: subprocess.Popen[str] | None = None
+        self.running = False
+        self.returncode = None
+        self.pid = None
+        self.cwd = os.path.expanduser()
+
+    def _restore(self) -> None:
+        self.stdout = subprocess.PIPE
+        self.stderr = subprocess.PIPE
+        self.running = self.returncode = self.pid = None
+
+    def __call__(self, args: str) -> None:
+        try:
+            self.proc: subprocess.Popen[str] = subprocess.Popen(
+                args=shlex.split(args),
+                stdout=self.stdout,
+                stderr=self.stderr,
+                shell=False,
+                text=True,
+                cwd=os.getcwd(),
+            )
+            with self.proc as p:
+                self.running = True
+                self.stdout, self.stderr = p.communicate(timeout=10)
+                self.pid = p.pid
+                self.returncode = p.returncode
+                self.running = False
+        except ValueError:
+            logging.error("Command <command> failed malformed command call ")
+        except subprocess.TimeoutExpired:
+            self.proc.kill()
+            logging.error("Command <command> failed: timeout")
+            self.stdout, self.stderr = self.proc.communicate()
+        except subprocess.SubprocessError as e:
+            logging.error("Command <command> failed: %s ", e)
