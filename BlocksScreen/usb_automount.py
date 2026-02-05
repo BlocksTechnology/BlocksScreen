39271b45e0ae870f6c0057e6e1b1cc0fa1ed397c
HugoCLSC <hugo.santos.costa@gmail.com>
2026-02-05 10:58:35 +0000

    WIP: Add UDisks2 Partition Interface, properties changed signal handling
    The properties of a device although uncommon may change,
    so a handler for the signal UDisks2 manager `properties_changed`
    was added. The logic is still missing here.
    The initial representation of a Device is added but not finished

--- BlocksScreen/usb_automount.py
+++ BlocksScreen/usb_automount.py
@@ -14,6 +14,18 @@ from PyQt6 import QtCore, QtWidgets
 # TODO: Add restart UDisksDBusAsync thread, instantiate again or just create methods inside that class that make it restart itself
 
 
+# NOTE: If the HINTSYSTEM == true then i should ignore because the partition may have OS sensitive partitions.NotImplementedError(
+#
+# )
+class Device:
+    def __init__(self, path: str) -> None:
+        self.path: str = path
+        self.partitions: dict[str, dict] = {}
+
+    def update(self, data: dict) -> None:
+        pass
+
+
 class USBManager(QtCore.QObject):
     usb_add: typing.ClassVar[QtCore.pyqtSignal] = QtCore.pyqtSignal(
         str, str, name="usb-add"
@@ -24,10 +36,8 @@ class USBManager(QtCore.QObject):
 
     def __init__(self, parent: QtCore.QObject, gcodes_dir: str, mnt_dir: str) -> None:
         super().__init__(parent)
-
         if not (os.path.isdir(gcodes_dir) and os.path.exists(gcodes_dir)):
             logging.info("Provided gcodes directory does not exist.")
-
         self.udisks: UDisksDBusAsync = UDisksDBusAsync(
             parent=self, mnt_dir=mnt_dir, gcodes_dir=gcodes_dir
         )
@@ -37,9 +47,6 @@ class USBManager(QtCore.QObject):
 
     def restart(self) -> None:
         self.udisks.start(self.udisks.Priority.InheritPriority)
-
-
-DEV_TYPES: list[str] = ["sd"]
 
 
 class UDisksDBusAsync(QtCore.QThread):
@@ -68,7 +75,7 @@ class UDisksDBusAsync(QtCore.QThread):
         self.loop: asyncio.AbstractEventLoop | None = None
         self.stop_event: asyncio.Event = asyncio.Event()
         self.listener_running: bool = False
-        self.controlled_devs = {}
+        self.controlled_devs: dict[str, Device] = {}
 
     def run(self) -> None:
         """Start UDisks2 USB monitoring"""
@@ -90,7 +97,7 @@ class UDisksDBusAsync(QtCore.QThread):
                 self.stop_event.set()
                 self.loop.call_soon_threadsafe(self.loop.stop)
             self.quit()
-            self.wait()
+            _ = self.wait()
             self.deleteLater()
         except asyncio.CancelledError as e:
             logging.error(
@@ -104,20 +111,26 @@ class UDisksDBusAsync(QtCore.QThread):
         """
         add_listener = asyncio.create_task(self._add_interface_listener())
         rem_listener = asyncio.create_task(self._rem_interface_listener())
+        pchange_listener = asyncio.create_task(self._properties_changed_listener())
         self.task_stack.add(add_listener)
         self.task_stack.add(rem_listener)
+        self.task_stack.add(pchange_listener)
         while self.stop_event:
             try:
-                await asyncio.gather(add_listener, rem_listener)
+                await asyncio.gather(add_listener, rem_listener, pchange_listener)
                 add_listener.add_done_callback(
                     lambda _: self.task_stack.discard(add_listener)
                 )
                 rem_listener.add_done_callback(
                     lambda _: self.task_stack.discard(rem_listener)
                 )
+                pchange_listener.add_done_callback(
+                    lambda _: self.task_stack.discard(pchange_listener)
+                )
             except asyncio.CancelledError as e:
                 _ = add_listener.cancel()
                 _ = rem_listener.cancel()
+                _ = pchange_listener.cancel()
                 logging.error(
                     "Caught exception while starting UDisks2 interfaces listeners: %s",
                     e,
@@ -125,69 +138,63 @@ class UDisksDBusAsync(QtCore.QThread):
 
     async def _add_interface_listener(self) -> None:
         async for path, interfaces in self.obj_manager.interfaces_added:
-            # Drivers such as USB devices will always have a name like sdx
-            # -> sd represents device driver type meaning SCSI Disk
-            # -> Device indexes are represented by the x on sdx, which is the order by which the kernel dicovers the drive.
-            #    x can have any letter [a-z]
-            kdn: str = pathlib.Path(path).stem
-            print(kdn)
-            str.count
-            # if not kdn.count(DEV_TYPES):
-            if 
-                # if not kdn.count(DEV_TYPES) in "sd":
-                print("Device not accepted")
+            if "org.freedesktop.Udisks2.Drive" in interfaces:
+                dev = Device(path)
+                self.controlled_devs.update({path: dev})
                 continue
-            dindex: str = kdn.rsplit("sd")[1]
-            print("Device detected")
             if "org.freedesktop.UDisks2.Block" in interfaces:
-                self.device_added.emit(path, interfaces)
+                print("Reconizing block now ")
                 bdev: UDisks2BlockAsyncInterface = UDisks2BlockAsyncInterface.new_proxy(
                     service_name="org.freedesktop.UDisks2",
                     object_path=path,
                     bus=self.system_bus,
                 )
+                drv_path = await bdev.drive
+                print(drv_path)
+                if drv_path in self.controlled_devs:
+                    print("Driver in controlled devs")
+                    dev: Device = self.controlled_devs[drv_path]
+                    if "org.freedesktop.UDisks2.Filesystem" in interfaces:
+                        devfs: UDisks2FileSystemAsyncInterface = (
+                            UDisks2FileSystemAsyncInterface.new_proxy(
+                                service_name="org.freedesktop.UDisks2",
+                                object_path=path,
+                                bus=self.system_bus,
+                            )
+                        )
+                        devfs.properties_changed
+                        continue
+                    if "org.freedesktop.UDisks2.Partition" in interfaces:
+                        print("Driver has partition ")
+                        devpart: UDisks2PartitionAsyncInterface = (
+                            UDisks2PartitionAsyncInterface.new_proxy(
+                                service_name="org.freedesktop.UDisks2",
+                                object_path=path,
+                                bus=self.system_bus,
+                            )
+                        )
+                        continue
 
-                drive_path, id_label = await asyncio.gather(bdev.drive, bdev.id_label)
-                if not drive_path:
-                    continue
-                if not id_label:
-                    # TODO: here signal that a drive might be mounted on sdx<number>
-                    pass
-                # ddev: UDisks2DriveAsyncInterface = UDisks2DriveAsyncInterface.new_proxy(
-                #     service_name="org.freedesktop.UDisks2",
-                #     object_path=drive_path,
-                #     bus=self.system_bus,
-                # )
-                # vendor_name = await ddev.vendor
-                # con_bus, removable, media_removable = await asyncio.gather(
-                #   ddev.connection_bus,
-                #   ddev.removable,
-                #   ddev.media_removable,
-                # )
-
-                # if (
-                #     con_bus.lower() == "usb"
-                # ):  # FIX: I NEED TO WAY FOR UDISKS TO RECEIVE THAT THE PARTITION IS ALSO ADDED
-                #     if removable and media_removable:
-                #         symlink_path: pathlib.Path = self.gcodes_path.as_posix()
-                #         # symlink_path.joinpath(id label after ) # TODO: Create the correct path, -> create the required symlink
-                #         # print(symlink_path)
-                #         # symlink_path =  symlink_path.
-                #         # self.add_symlink(pathlib.Path(self.gcodes_path.as_posix()).absolute()))
-                #
+    async def _properties_changed_listener(self) -> None:
+        async for (
+            path,
+            changed_properties,
+            invalid_properties,
+        ) in self.obj_manager.properties_changed:
+            print(path)
+            print(changed_properties)
+            print(invalid_properties)
 
     async def _rem_interface_listener(self) -> None:
         async for path, interfaces in self.obj_manager.interfaces_removed:
             if "org.freedesktop.UDisks2.Block" in interfaces:
                 os.rmdir("/home/bugo/printer_data/gcodes/USB_PEN")
 
-    def add_symlink(self, path: str) -> None:
+    def add_symlink(self, path: str, label: str | None) -> None:
         """Create symlink on `path`"""
         if not (os.path.exists(path) or os.path.islink(path)):
-            os.symlink(
-                src="/media/bugo/BLOCKS",
-                dst="/home/bugo/printer_data/gcodes/USB_PEN",
-            )
+            dstb = pathlib.Path(path).joinpath(label if label else "USB DRIVE")
+            os.symlink(src="/media/bugo/BLOCKS", dst=dstb)
 
     def rem_symlink(self, path: str) -> None:
         """Remove symlink located in `path`"""
@@ -218,6 +225,65 @@ class UDisks2Manager(sdbus.DbusObjectManagerInterface):
         bus: sdbus.SdBus | None = None,
     ):
         super().__init__(service_name, object_path, bus)
+
+
+class UDisks2PartitionAsyncInterface(
+    sdbus.DbusInterfaceCommonAsync, interface_name="org.freedesktop.UDisks2.Partition"
+):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @sdbus.dbus_method_async(input_signature="s")
+    async def set_type(self, type: str) -> None:
+        raise NotImplementedError
+
+    @sdbus.dbus_method_async(input_signature="s")
+    async def set_name(self, name: str) -> None:
+        raise NotImplementedError
+
+    @sdbus.dbus_method_async(input_signature="a{sv}")
+    async def delete(self, opts: dict) -> None:
+        raise NotImplementedError
+
+    @sdbus.dbus_property_async(property_signature="u")
+    def number(self) -> int:
+        raise NotImplementedError
+
+    @sdbus.dbus_property_async(property_signature="s")
+    def type(self) -> str:
+        raise NotImplementedError
+
+    @sdbus.dbus_property_async(property_signature="t")
+    def flags(self) -> int:
+        raise NotImplementedError
+
+    @sdbus.dbus_property_async(property_signature="t")
+    def offset(self) -> int:
+        raise NotImplementedError
+
+    @sdbus.dbus_property_async(property_signature="t")
+    def size(self) -> int:
+        raise NotImplementedError
+
+    @sdbus.dbus_property_async(property_signature="s")
+    def name(self) -> str:
+        raise NotImplementedError
+
+    @sdbus.dbus_property_async(property_signature="s")
+    def uuid(self) -> str:
+        raise NotImplementedError
+
+    @sdbus.dbus_property_async(property_signature="o")
+    def table(self) -> str:
+        raise NotImplementedError
+
+    @sdbus.dbus_property_async(property_signature="b")
+    def is_container(self) -> bytes:
+        raise NotImplementedError
+
+    @sdbus.dbus_property_async(property_signature="b")
+    def is_contained(self) -> bytes:
+        raise NotImplementedError
 
 
 class UDisks2FileSystemAsyncInterface(
@@ -477,19 +543,6 @@ class MainWindow(QtWidgets.QMainWindow):
             mnt_dir="/home/bugo/printer_data/gcodes/",
             gcodes_dir="/media/",
         )
-
-
-# async def start() -> None:
-# bus = sdbus.sd_bus_open_system()
-# fs = UDisks2BlockAsyncInterface().new_proxy(
-#     "org.freedesktop.UDisks2", "/org/freedesktop/UDisks2/block_devices/sdd", bus
-# )
-#
-# id = await fs.device_number
-# device = await fs.device
-# print(id)
-# print(device)
-# manager = UDisks2DBusAsync()
 
 
 if __name__ == "__main__":
