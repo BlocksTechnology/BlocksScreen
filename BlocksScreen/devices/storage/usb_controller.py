ff7e5fc6e25b0605dca4ea8b7b28aeb90fb4b084
HugoCLSC <hugo.santos.costa@gmail.com>
2026-02-12 11:01:18 +0000

    WIP print to udisks2 signals

--- BlocksScreen/devices/storage/usb_controller.py
+++ BlocksScreen/devices/storage/usb_controller.py
@@ -1,11 +1,9 @@
 import logging
 import os
-import sys
 import typing
-from PyQt6 import QtCore, QtWidgets
+from PyQt6 import QtCore
 
 from .udisks2 import UDisksDBusAsync
-
 
 class USBManager(QtCore.QObject):
     usb_add: typing.ClassVar[QtCore.pyqtSignal] = QtCore.pyqtSignal(
@@ -17,35 +15,23 @@ class USBManager(QtCore.QObject):
 
     def __init__(self, parent: QtCore.QObject, gcodes_dir: str, mnt_dir: str) -> None:
         super().__init__(parent)
-        if not (os.path.isdir(gcodes_dir) and os.path.exists(gcodes_dir)):
+        self.gcodes_dir: str = os.path.expanduser("~/printer_data/gcodes")
+        if not (os.path.isdir(self.gcodes_dir) and os.path.exists(self.gcodes_dir)):
             logging.info("Provided gcodes directory does not exist.")
         self.udisks: UDisksDBusAsync = UDisksDBusAsync(
-            parent=self, mnt_dir=mnt_dir, gcodes_dir=gcodes_dir
+            parent=self, gcodes_dir=self.gcodes_dir
         )
         self.udisks.start(self.udisks.Priority.InheritPriority)
-        # TODO:: self.udisks.finished.connect(self.restart)
-        # TODO:: self.udisks.started.connect( do somethign here)
+        self.udisks.hardware_detected.connect(self._handle_new_hardware)
+        self.udisks.hardware_removed.connect(self._handle_rem_hardware)
 
     def restart(self) -> None:
         self.udisks.start(self.udisks.Priority.InheritPriority)
 
+    @QtCore.pyqtSlot(str, name="hardware_detected")
+    def _handle_new_hardware(self, path: str):
+        print("New hardware detected")
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self) -> None:
-        super().__init__()
-        # udisks = UDisksDBus(self)
-        # udisks.start(priority=udisks.Priority.InheritPriority)
-
-        # print(udisks.currentThreadId())
-        udisks = USBManager(
-            parent=self,
-            mnt_dir="/home/bugo/printer_data/gcodes/",
-            gcodes_dir="/media/",
-        )
-
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    main_window = MainWindow()
-    app.processEvents()
-    sys.exit(app.exec())
+    @QtCore.pyqtSlot(str, name="hardware_removed")
+    def _handle_rem_hardware(self, path: str):
+        logging.("hardware  removed detected")
