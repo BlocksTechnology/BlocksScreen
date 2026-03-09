d40e683fce4353dcc6577d555a85f5c37571ff99
HugoCLSC <hugo.santos.costa@gmail.com>
2026-03-09 14:54:08 +0000

    Change method names, auto restart when usb monitor stops

--- BlocksScreen/devices/storage/usb_controller.py
+++ BlocksScreen/devices/storage/usb_controller.py
@@ -14,45 +14,51 @@ class USBManager(QtCore.QObject):
         str, str, name="usb-rem"
     )
 
-    def __init__(self, parent: QtCore.QObject, gcodes_dir: str, mnt_dir: str) -> None:
+    def __init__(self, parent: QtCore.QObject, gcodes_dir: str) -> None:
         super().__init__(parent)
-        self.gcodes_dir: str = os.path.expanduser("~/printer_data/gcodes")
+        self.gcodes_dir: str = gcodes_dir or os.path.expanduser("~/printer_data/gcodes")
         if not (os.path.isdir(self.gcodes_dir) and os.path.exists(self.gcodes_dir)):
             logging.info("Provided gcodes directory does not exist.")
         self.udisks: UDisksDBusAsync = UDisksDBusAsync(
             parent=self, gcodes_dir=self.gcodes_dir
         )
         self.udisks.start(self.udisks.Priority.InheritPriority)
-        self.udisks.hardware_detected.connect(self._handle_new_hardware)
-        self.udisks.hardware_removed.connect(self._handle_rem_hardware)
-        self.udisks.device_added.connect(self._handle_new_device)
-        self.udisks.device_removed.connect(self._handle_rem_device)
-        self.udisks.device_mounted.connect(self._handle_mounted_device)
-        self.udisks.device_unmounted.connect(self._handle_unmounted_device)
+        self.udisks.hardware_detected.connect(self.handle_new_hardware)
+        self.udisks.hardware_removed.connect(self.handle_rem_hardware)
+        self.udisks.device_added.connect(self.handle_new_device)
+        self.udisks.device_removed.connect(self.handle_rem_device)
+        self.udisks.device_mounted.connect(self.handle_mounted_device)
+        self.udisks.device_unmounted.connect(self.handle_unmounted_device)
+        self.udisks.finished.connect(self._handle_monitor_finished)
 
     def restart(self) -> None:
         self.udisks.start(self.udisks.Priority.InheritPriority)
 
+    @QtCore.pyqtSlot(name="finished")
+    def _handle_monitor_finished(self) -> None:
+        # Just restart the monitor for now
+        self.restart()
+
     @QtCore.pyqtSlot(str, str, name="device-mounted")
-    def _handle_mounted_device(self, path, symlink) -> None:
+    def handle_mounted_device(self, path, symlink) -> None:
         pass
 
     @QtCore.pyqtSlot(str, name="device-unmounted")
-    def _handle_unmounted_device(self, path) -> None:
+    def handle_unmounted_device(self, path) -> None:
         pass
 
     @QtCore.pyqtSlot(str, dict, name="device-added")
-    def _handle_new_device(self, path, interface) -> None:
+    def handle_new_device(self, path, interface) -> None:
         pass
 
     @QtCore.pyqtSlot(str, name="device-removed")
-    def _handle_rem_device(self, path) -> None:
+    def handle_rem_device(self, path) -> None:
         pass
 
     @QtCore.pyqtSlot(str, name="hardware_detected")
-    def _handle_new_hardware(self, path: str):
+    def handle_new_hardware(self, path: str):
         pass
 
     @QtCore.pyqtSlot(str, name="hardware_removed")
-    def _handle_rem_hardware(self, path: str):
+    def handle_rem_hardware(self, path: str):
         pass
