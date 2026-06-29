e9452d3f8e1927b292c654c17c5a76837bcb2ea2
Roberto Martins <roberto.martins@blockstec.com>
2026-06-29 10:01:10 +0100

    bugfix: resume button logic

--- BlocksScreen/lib/panels/widgets/jobStatusPage.py
+++ BlocksScreen/lib/panels/widgets/jobStatusPage.py
@@ -8,8 +8,8 @@ from lib.utils.blocks_button import BlocksCustomButton
 from lib.utils.blocks_label import BlocksLabel
 from lib.utils.blocks_progressbar import CustomProgressBar
 from lib.utils.display_button import DisplayButton
-from PyQt6 import QtCore, QtGui, QtWidgets
 from lib.utils.flowguard import FlowguardWidget
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 logger = logging.getLogger(__name__)
 
@@ -197,36 +197,38 @@ class JobStatusWidget(QtWidgets.QWidget):
         self.file_metadata = fileinfo
         self._load_thumbnails(*fileinfo.get("thumbnail_images", []))
 
-    @QtCore.pyqtSlot(name="pause_resume_print")
     def pause_resume_print(self) -> None:
         """Handle pause/resume print job button clicked"""
         self.pause_printing_btn.setEnabled(False)
         if self._internal_print_status == "printing":
-            self._internal_print_status = "paused"
             self.print_pause.emit()
-        elif self._internal_print_status == "paused":
-            self._internal_print_status = "printing"
+        if self._internal_print_status == "paused":
             self.print_resume.emit()
 
     def _handle_print_state(self, state: str) -> None:
         """Handle print state change received from
         printer_status object updated
         """
-        valid_states = {"printing", "paused"}
+        valid_states = {"printing"}
         invalid_states = {"cancelled", "complete", "error", "standby"}
         lstate = state.lower()
-        if lstate in valid_states:
+
+        if lstate == "paused":
+            self.pause_printing_btn.setEnabled(True)
+            self.pause_printing_btn.setText("Resume")
+            self.pause_printing_btn.setPixmap(
+                QtGui.QPixmap(":/ui/media/btn_icons/play.svg")
+            )
             self._internal_print_status = lstate
-            if lstate == "paused":
-                self.pause_printing_btn.setText(" Resume")
-                self.pause_printing_btn.setPixmap(
-                    QtGui.QPixmap(":/ui/media/btn_icons/play.svg")
-                )
-            elif lstate == "printing":
-                self.pause_printing_btn.setText("Pause")
-                self.pause_printing_btn.setPixmap(
-                    QtGui.QPixmap(":/ui/media/btn_icons/pause.svg")
-                )
+        elif lstate == "printing":
+            self.pause_printing_btn.setEnabled(True)
+            self.pause_printing_btn.setText("Pause")
+            self.pause_printing_btn.setPixmap(
+                QtGui.QPixmap(":/ui/media/btn_icons/pause.svg")
+            )
+            self._internal_print_status = lstate
+
+        if lstate in valid_states:
             self.pause_printing_btn.setEnabled(True)
             self.request_query_print_stats.emit({"print_stats": ["filename"]})
             self.call_cancel_panel.emit(False)
