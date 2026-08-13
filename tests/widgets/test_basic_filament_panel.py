a85ca3ae24cbd4bf22a0bebd259e4964dbf86df4
Robert0Mart <robertomicael.martins@gmail.com>
2026-08-13 17:17:59 +0100

    upd: updated path

--- tests/widgets/test_basic_filament_panel.py
+++ tests/widgets/test_basic_filament_panel.py
@@ -40,7 +40,7 @@ sys.modules.setdefault("lib.utils.blocks_button", _button_stub)
 
 import pytest  # noqa: E402
 
-from BlocksScreen.lib.panels.widgets.basicFilamentPanel import (  # noqa: E402
+from BlocksScreen.lib.panels.widgets.FilamentTab.basicFilamentPanel import (  # noqa: E402
     BasicFilamentPanel,
 )
 
